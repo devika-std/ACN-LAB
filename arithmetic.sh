a2539ccc87db4d7094385dd507a29e7a0d8b3c38
devika-std <devikasuresh26@gmail.com>
2025-04-25 15:54:42 +0530

    Add files via upload

---
+++ arithmetic.sh
@@ -0,0 +1,12 @@
+echo "Enter two numbers"
+read x
+read y
+c=$((x+y))
+d=$((x-y))
+e=$((x*y))
+g=$((x/y))
+echo "The results are :"
+echo "The sum is :$c "
+echo "The difference:$d"
+echo "The product:$e"
+echo "The quotient :$g"
